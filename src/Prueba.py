28539b49675d1ae9d4bf8d23e8d5e5140e60cbca
nanperez <perez.castro.nancy@gmail.com>
2024-08-29 18:12:45 -0600

    CODIGOS NUEVOS m

--- src/Prueba.py
+++ src/Prueba.py
@@ -176,7 +176,8 @@ with open(ruta1, 'w') as f:
      ## Convertir el conjunto de validación en tensor y aplicar `datagen_val_test`
      #val_data_fold = datagen_val_test.flow(val_images_fold, val_labels_fold, batch_size=batch_size, shuffle=False)
         
-
+     class_counts = train_fold_generator.class_indices
+     print(class_counts)
      #Estructura del modelo
      model.compile(optimizer=Adam(learning_rate=rate), #se emplea el optimizador Adam con tasa de aprendizaje 0.001
                       loss=BinaryCrossentropy(from_logits=False),   # función de pérdida
@@ -187,7 +188,7 @@ with open(ruta1, 'w') as f:
      history = model.fit(
         train_fold_generator,
         epochs=epochs,  # Número de épocas de entrenamiento
-        validation_data=val_data_fold
+        validation_data=val_data_fold, shuffle=True
      )
      
      modelos.append(model)
