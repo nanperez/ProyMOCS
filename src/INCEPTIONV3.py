267ddec4fde3efb0f50d6788b7826702357e9e39
nanperez <perez.castro.nancy@gmail.com>
2024-09-02 10:09:54 -0600

    CODIGOS NUEVOS m

--- src/INCEPTIONV3.py
+++ src/INCEPTIONV3.py
@@ -26,8 +26,8 @@ from collections import Counter
 img_height,img_width = 299,299 # tamaño de redimension de lasi magenes
 rate = 0.001 # taza de aprendizaje para el entrenamiento
 batch_size = 32 # tamaño de lote
-epochs = 500 # epocas para el entrenamiento
-ejecucion=5
+epochs = 1000 # epocas para el entrenamiento
+ejecucion=1
 #Funcion del modelo base 
 def create_modelo_base():
      modelo_base=InceptionV3(include_top=False,
